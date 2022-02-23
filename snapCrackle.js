5c3b423570b4c67ffde63dd41d0b97e7df10a70f
Joao Gabriel Pawlick <JGPawlick@gmail.com>
2022-02-23 18:30:40 -0300

    Entrega
    Entrega de acordo com o pedido

---
+++ snapCrackle.js
@@ -0,0 +1,22 @@
+function snapCrackle(maxValue) {
+  let myString = "";
+  //let myArray = []
+  // Acretido que em vez de string poderia usar array de maneira mais pratica e facil, porrem ainda estou tendo dificuldades com array, então vai string mesmo.
+  for (let valueCounter = 1; valueCounter <= maxValue; valueCounter++) {
+
+    if (valueCounter % 2 != 0 && valueCounter % 5 == 0) {
+      myString += "SnapCrackle , ";
+
+    } else if (valueCounter % 5 == 0) {
+      myString += "Crackle , ";
+
+    } else if (valueCounter % 2 != 0) {
+      myString += "Snap , ";
+
+    } else if (valueCounter % valueCounter == 0 && valueCounter % 1 == 0) {
+      myString += valueCounter + " , ";
+    }
+
+  }
+  return myString
+}
